ec3709337a9b10db39aee77151b609081edd8c99
Shamal Muneer <shamal20017@gmail.com>
2024-06-03 04:05:46 +0530

    fix: complete chat

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,11 @@
+import logo from "/logo.png";
 interface Props {
   className?: string;
 }
 function Logo({ className }: Props) {
   return (
     // <div className="flex h-full items-center">
-    <img className={`${className}`} src="\Logo.png"></img>
+    <img className={`${className}`} src={logo}></img>
     // </div>
   );
 }
